a56be9a8d8752fe5c30ee5ab7029b89ded26c3a9
chaudhary64 <chiragchoudhary64@gmail.com>
2024-05-28 20:04:41 +0530

    ```text refactor: Update Button component styles and font
    This commit refactors the Button component in the src/Components/Button.jsx file. The styles have been updated to use a smaller width and height, and the font has been changed to SmoochSans-Light. This change improves the visual appearance of the button.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,8 +3,8 @@ import { MdSubdirectoryArrowRight } from "react-icons/md";
 
 const Button = ({ text = "Live Website" }) => {
   return (
-    <div className="h-9 w-40 rounded-full flex justify-evenly items-center bg-white">
-      <p className="text-lg tracking-wide">{text}</p>
+    <div className="h-8 w-36 rounded-full flex justify-evenly items-center bg-white">
+      <p className="text-lg tracking-wide font-[SmoochSans-Light]">{text}</p>
       <MdSubdirectoryArrowRight style={{ transform: "translateY(15%)" }} />
     </div>
   );
