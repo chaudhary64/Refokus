444394f0800476fd84ecf6100f4c6447ead54d2d
chaudhary64 <chiragchoudhary64@gmail.com>
2024-05-29 19:32:14 +0530

    Give the Work and Nav Component width : 100% and wrap the Work Component in fragments

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Work = () => {
   return (
-    <div className="h-[90vh] w-screen py-5 bg-black flex flex-col justify-end items-center gap-10">
-      <p className="text-[37vw] font-[Raleway-Medium] text-white font-thin tracking-tight leading-[0.8] select-none overflow-hidden">
-        work
-      </p>
-      <p className="text-2xl text-[#646464] font-[SmoochSans-Light]">
-        Web Design, Webflow Development, Creative Development
-      </p>
-    </div>
+    <>
+      <div className="h-[90vh] w-full py-5 bg-black flex flex-col justify-end items-center gap-10">
+        <p className="text-[37vw] font-[Raleway-Medium] text-white font-thin tracking-tight leading-[0.8] select-none overflow-hidden">
+          work
+        </p>
+        <p className="text-2xl text-[#646464] font-[SmoochSans-Light]">
+          Web Design, Webflow Development, Creative Development
+        </p>
+      </div>
+    </>
   );
 };
 
