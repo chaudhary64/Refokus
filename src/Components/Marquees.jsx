53b1df2d84fb4b06d240265ab3481a997339b120
chaudhary64 <chiragchoudhary64@gmail.com>
2024-05-29 19:22:20 +0530

    Created the data fro marquee Animation and removed the unnessary import from work component

---
+++ src/Components/Marquees.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Marquees = () => {
+  const marqueeData = [
+    {
+      id: 1,
+      number: 9,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg",
+    },
+    {
+      id: 2,
+      number: 8,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63d6e83420934a94d642103b_NCC2021_LogoLockup%201.svg",
+    },
+    {
+      id: 3,
+      number: 2,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd75d563689fd_Awwards-logotype-2018%201.svg",
+    },
+    {
+      id: 4,
+      number: 9,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg",
+    },
+    {
+      id: 5,
+      number: 8,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63d6e83420934a94d642103b_NCC2021_LogoLockup%201.svg",
+    },
+  ];
+  return <div></div>;
+};
+
+export default Marquees;
