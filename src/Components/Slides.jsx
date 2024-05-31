d4757cc2b217114c879e7319660ddc5bf57cd555
chaudhary64 <chiragchoudhary64@gmail.com>
2024-05-31 16:09:27 +0530

    refactor: Update Slides component styles and structure
    This commit refactors the Slides component in the src/Components/Slides.jsx file. It updates the font style to 'Satoshi-Regular' and adjusts the width and height of the component. The description text is now wider and the buttons are displayed in a row. These changes improve the visual presentation and readability of the slides.

--- src/Components/Slides.jsx
+++ src/Components/Slides.jsx
@@ -4,13 +4,16 @@ import Button from "./Button";
 const Slides = ({ id, heading, description, caseStudy, bgColor }) => {
   console.log(id, heading, description, caseStudy, bgColor);
   return (
-    <div className="h-[25vh] w-full flex justify-between items-center">
+    <div className="h-[31vh] w-full font-[Satoshi-Regular] flex justify-between items-center">
       <span className="flex text-white w-[65%] text-5xl">
         <p>{heading}</p>
       </span>
-      <div className="h-full w-[35%] flex flex-col justify-evenly border-2 border-cyan-400">
-        <p className="text-white text-[18px] w-[48%]">{description}</p>
-        {caseStudy ? <Button text={"Case Study"} /> : <Button />}
+      <div className="w-[35%] flex flex-col justify-evenly gap-5">
+        <p className="text-white text-[18px] w-[69%]">{description}</p>
+        <span className="flex gap-5">
+          <Button />
+          {caseStudy && <Button text={"Case Study"} />}
+        </span>
       </div>
     </div>
   );
