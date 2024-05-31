bc9d2678a7511218c002aa0aa9d5c99aa7e69c0e
chaudhary64 <chiragchoudhary64@gmail.com>
2024-05-31 16:55:46 +0530

    Update Slides component styles and structure

--- src/Components/Slides.jsx
+++ src/Components/Slides.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Button from "./Button";
+import { useState } from "react";
 
 const Slides = ({ id, heading, description, caseStudy, bgColor }) => {
-  console.log(id, heading, description, caseStudy, bgColor);
+  const [hide, setHide] = useState(true);
   return (
-    <div className="h-[31vh] w-full font-[Satoshi-Regular] flex justify-between items-center">
+    <div className="h-[37vh] w-full font-[Satoshi-Regular] flex justify-between items-center">
       <span className="flex text-white w-[65%] text-5xl">
         <p>{heading}</p>
       </span>
       <div className="w-[35%] flex flex-col justify-evenly gap-5">
         <p className="text-white text-[18px] w-[69%]">{description}</p>
-        <span className="flex gap-5">
+        <span className={`flex gap-5 ${hide ? "opacity-0" : "opacity-1"}`}>
           <Button />
           {caseStudy && <Button text={"Case Study"} />}
         </span>
