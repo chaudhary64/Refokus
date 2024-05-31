5dea4569213991f2039bc38b38c539bc6869dcff
chaudhary64 <chiragchoudhary64@gmail.com>
2024-05-31 14:16:15 +0530

    Changed the border Color of Marquee

--- src/Components/Marquee.jsx
+++ src/Components/Marquee.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 const Marquee = ({ id, length, src, number }) => {
   const width = 100 / length;
-  //   console.log(id);
   return (
     <div
       style={{ width: width + "%" }}
-      className={`h-full bg-slate-900 ${
+      className={`h-full ${
         id == 1 ? "border-b-2 border-t-2 border-r-2" : "border-2"
-      } border-white flex justify-evenly items-center`}
+      } border-[#3E3E46] flex justify-evenly items-center`}
     >
       <img src={`${src}`} alt="" />
       <p className="text-4xl text-white">{number}</p>
