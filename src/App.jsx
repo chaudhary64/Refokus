119d4c61edd0d7406fa002a6dd31c887965ac5e7
chaudhary64 <chiragchoudhary64@gmail.com>
2024-06-01 18:40:58 +0530

    Revert "Added Locomotive"
    This reverts commit c7c7e5c2f3be57f9de61d7fb6c7af65b2ba6b2a1.

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
-import React, { useEffect, useRef } from "react";
-import { useLocation } from "react-router-dom";
-import LocomotiveScroll from "locomotive-scroll";
+import React from "react";
+import Home from "./Components/Home";
 import Nav from "./Components/Nav";
 import RoutesForNav from "./Components/RoutesForNav";
 
 const App = () => {
-  const location = useLocation();
-  const scrollRef = useRef(null);
-
-  useEffect(() => {
-    // Initialize or re-initialize Locomotive Scroll
-    const scrollElement = document.querySelector("[data-scroll-container]");
-    if (scrollRef.current) {
-      scrollRef.current.destroy();
-    }
-
-    scrollRef.current = new LocomotiveScroll({
-      el: scrollElement,
-      smooth: true,
-    });
-
-    // Update the instance on route change
-    scrollRef.current.update();
-
-    // Cleanup on component unmount
-    return () => {
-      if (scrollRef.current) {
-        scrollRef.current.destroy();
-      }
-    };
-  }, [location.pathname]);
-
   return (
-    <div data-scroll-container>
+    <>
       <Nav />
       <RoutesForNav />
-    </div>
+    </>
   );
 };
 
