69e7faf7ca40e32cda2ace099ac07dc5f3f79b82
chaudhary64 <chiragchoudhary64@gmail.com>
2024-06-01 18:59:02 +0530

    Made one Infinite Marquee Animation with the help of Framer Motion

--- src/Components/Marquees.jsx
+++ src/Components/Marquees.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Marquee from "./Marquee";
+import { MotionConfig, motion } from "framer-motion";
 
 const Marquees = ({
   marqueeDataWithNumber: marqueeData,
@@ -9,34 +10,46 @@ const Marquees = ({
   return marqueeWithNumber ? (
     // Marquee with number
     <div className="h-[10vh] w-full mt-[7vh] flex flex-nowrap overflow-hidden">
-      {/* First Marquee Conatiner */}
-      <div className="h-full w-full flex shrink-0">
-        {marqueeData.map((marquee) => {
-          return (
-            <Marquee
-              key={marquee.id}
-              id={marquee.id}
-              length={marqueeData.length}
-              src={marquee.src}
-              number={marquee.number}
-            />
-          );
-        })}
-      </div>
-      {/* Second Marquee Conatiner */}
-      <div className="h-full w-full flex shrink-0">
-        {marqueeData.map((marquee) => {
-          return (
-            <Marquee
-              key={marquee.id}
-              id={marquee.id}
-              length={marqueeData.length}
-              src={marquee.src}
-              number={marquee.number}
-            />
-          );
-        })}
-      </div>
+      <MotionConfig
+        animate={{
+          x: ["0%", "-100%"], // Move from 0% to -100% of the container width
+          transition: {
+            duration: 15,
+            repeat: Infinity,
+            repeatType: "loop",
+            ease: "linear",
+          },
+        }}
+      >
+        {/* First Marquee Conatiner */}
+        <motion.div className="h-full w-full flex shrink-0">
+          {marqueeData.map((marquee) => {
+            return (
+              <Marquee
+                key={marquee.id}
+                id={marquee.id}
+                length={marqueeData.length}
+                src={marquee.src}
+                number={marquee.number}
+              />
+            );
+          })}
+        </motion.div>
+        {/* Second Marquee Conatiner */}
+        <motion.div className="h-full w-full flex shrink-0">
+          {marqueeData.map((marquee) => {
+            return (
+              <Marquee
+                key={marquee.id}
+                id={marquee.id}
+                length={marqueeData.length}
+                src={marquee.src}
+                number={marquee.number}
+              />
+            );
+          })}
+        </motion.div>
+      </MotionConfig>
     </div>
   ) : (
     // Marquee without number
