c1df7e9582447b59c033cea772b7644bda7d9416
chaudhary64 <chiragchoudhary64@gmail.com>
2024-06-20 21:31:53 +0530

    refactor: Update Work component markup and styles
    This commit refactors the Work component in the src/Components/Work.jsx file. It updates the markup and styles of the component, replacing the div element with a section element for better semantic structure.This change improves the readability and maintainability of the code.

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -116,14 +116,14 @@ const Work = () => {
   ];
   return (
     <>
-      <div className="h-[90vh] w-full py-5 flex flex-col justify-end items-center gap-10">
+      <section className="h-[90vh] w-full py-5 flex flex-col justify-end items-center gap-10">
         <p className="text-[37vw] font-[Raleway-Medium] text-white font-thin tracking-tight leading-[0.8] select-none overflow-hidden">
           work
         </p>
         <p className="text-2xl text-[#646464] font-[SmoochSans-Light]">
           Web Design, Webflow Development, Creative Development
         </p>
-      </div>
+      </section>
       <Marquees
         marqueeDataWithNumber={marqueeDataWithNumber}
         marqueeWithNumber={true}
