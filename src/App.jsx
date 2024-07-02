cfff17009154406c6fb38ec63b88ecd36fae97a8
chaudhary64 <chiragchoudhary64@gmail.com>
2024-07-02 22:53:00 +0530

    feat: Add lenis library for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Nav from "./Components/Nav/Nav.jsx";
 import RoutesForNav from "./Components/Nav/RoutesForNav.jsx";
+import { ReactLenis, useLenis } from "lenis/react";
 
 const App = () => {
   return (
-    <>
-      <Nav />
-      <RoutesForNav />
-    </>
+    <ReactLenis root>
+      <main>
+        <Nav />
+        <RoutesForNav />
+      </main>
+    </ReactLenis>
   );
 };
 
