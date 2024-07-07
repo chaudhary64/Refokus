71b6d38ad9232210622622a95312597f2a358a1a
chaudhary64 <chiragchoudhary64@gmail.com>
2024-07-07 16:08:36 +0530

    Implemented AnimatePresence from framer-motion in the RoutesForNav component to enable smooth page transitions. Wrapped the Routes component with AnimatePresence to handle exit animations based on route changes. Passed the location prop to Routes and used key={location.pathname} for dynamic transitions.

--- src/Components/Nav/RoutesForNav.jsx
+++ src/Components/Nav/RoutesForNav.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "../Home/Home";
 import Work from "../Work/Work";
 import About from "../About/About";
 import News from "../News/News";
 import Carrers from "../Carrers/Carrers";
+import { AnimatePresence } from "framer-motion";
 
 const RoutesForNav = () => {
+  const location = useLocation();
   return (
-    <div>
-      <Routes>
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/work" element={<Work />} />
         <Route path="/about" element={<About />} />
         <Route path="/news" element={<News />} />
         <Route path="/carrers" element={<Carrers />} />
       </Routes>
-    </div>
+    </AnimatePresence>
   );
 };
 
