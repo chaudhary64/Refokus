37100289de547e4d6956806acfd411c31564c20d
chaudhary64 <chiragchoudhary64@gmail.com>
2024-07-27 22:12:24 +0530

    feat: Add Marquee component to Home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -87,6 +87,33 @@ const Home = () => {
       },
     ],
   ];
+  const marqueeDataWithNumber = [
+    {
+      id: 1,
+      number: 9,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg",
+    },
+    {
+      id: 2,
+      number: 8,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63d6e83420934a94d642103b_NCC2021_LogoLockup%201.svg",
+    },
+    {
+      id: 3,
+      number: 2,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd75d563689fd_Awwards-logotype-2018%201.svg",
+    },
+    {
+      id: 4,
+      number: 9,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg",
+    },
+    {
+      id: 5,
+      number: 8,
+      src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63d6e83420934a94d642103b_NCC2021_LogoLockup%201.svg",
+    },
+  ];
   const overviewWorkData = [
     {
       id: 1,
@@ -215,6 +242,10 @@ const Home = () => {
           <Button text={"View All Work"} bgColor="transparent" />
         </Link>
       </section>
+      <Marquees
+        marqueeDataWithNumber={marqueeDataWithNumber}
+        marqueeWithNumber={true}
+      />
     </>
   );
 };
