32a75c6547595206f2951be1cce4dbc6b3bb5a3a
chaudhary64 <chiragchoudhary64@gmail.com>
2024-07-27 19:05:07 +0530

    Implemented the text part

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='h-[94vh] w-full bg-slate-600 text-9xl flex justify-center items-center overflow-hidden'>I am Home
-    </div>
-  )
-}
+    <>
+      <section className="h-fit w-[95%] mx-auto pt-20 text-white">
+        <div className="text-9xl leading-[0.95]">
+          <span>
+            <p>We Develop</p>
+          </span>
+          <span>
+            <p>
+              <span
+                style={{
+                  color: "transparent",
+                  background:
+                    "linear-gradient(90deg, rgba(18,245,197,1) 0%, rgba(213,35,238,1) 40%, rgba(216,32,238,1) 60%, rgba(0,212,255,1) 100%)",
+                  backgroundClip: "text",
+                }}
+                className="inline-block h-fit text-transparent"
+              >
+                amazing
+              </span>
+              websites
+            </p>
+          </span>
+          <span>
+            <p>
+              for your business <span>.</span>
+            </p>
+          </span>
+        </div>
+        <span className="mt-16 text-xl inline-block h-fit w-fit tracking-wider">
+          <p>
+            Refokus is a forward-thinking web agency that combines Web Design,
+            Webflow Development, and Creative Development to create cool
+            websites that help brands position themselves as market leaders.
+          </p>
+        </span>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
