23e3f45462912fdc94ca30bf014c101ef6aa1e9c
chaudhary64 <chiragchoudhary64@gmail.com>
2024-07-27 19:07:26 +0530

    feat: Add Marquee component to Home page
    Add the Marquee component to the Home page to display a scrolling carousel of images. This enhances the visual appeal and engagement of the page. The Marquee component is implemented with two sets of images, each set containing multiple logos. The Marquee component is added within a new section with the id "BrandMarquees" and is styled with a margin-top of 20vh and a flex layout. The Marquee component is used twice, once with the marqueeDataWithoutNumber array from the first set of images and once with the marqueeDataWithoutNumber array from the second set of images. Additionally, the second Marquee component is set to scroll in reverse. This change improves the overall user experience and adds dynamic visual content to the Home page.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,89 @@
 import React from "react";
+import Marquees from "../Marquee/Marquees";
 
 const Home = () => {
+  const marqueeDataWithoutNumber = [
+    [
+      {
+        id: 1,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275e12177716cb3f2ea_basf.svg",
+      },
+      {
+        id: 2,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27505e3a120466b87aa_singularity.svg",
+      },
+      {
+        id: 3,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275c2fcf540272672ef_intenseeye.svg",
+      },
+      {
+        id: 4,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27518bb52e4d642644e_bcgp.svg",
+      },
+      {
+        id: 5,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2756e8df24e52915c71_flowcode.svg",
+      },
+      {
+        id: 6,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275e1d5aa08ccc379dd_ypo.svg",
+      },
+      {
+        id: 7,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275909535ff431975cf_lavender.svg",
+      },
+      {
+        id: 8,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d36963b956910ca67534_remind.svg",
+      },
+      {
+        id: 9,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2ce14e6c85b010c2e1e3c_Logo%20White%201.svg",
+      },
+    ],
+    [
+      {
+        id: 1,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275a4f001b08a4e28bc_weglot.svg",
+      },
+      {
+        id: 2,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2754882567001f1ee80_spotify.svg",
+      },
+      {
+        id: 3,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275999da7719dc1fe2c_haufe.svg",
+      },
+      {
+        id: 4,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2759c8b021207af521b_yahoo.svg",
+      },
+      {
+        id: 5,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27565c41bb608f7f7c0_rainfall.svg",
+      },
+      {
+        id: 6,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275ff2309d9d743ec75_doxel.svg",
+      },
+      {
+        id: 7,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2755ba681134c9d9b5c_mural.svg",
+      },
+      {
+        id: 8,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/661b2455cb60f1919ab48e20_Logo.svg",
+      },
+      {
+        id: 9,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2753d48394d8adeff14_rocket.svg",
+      },
+      {
+        id: 10,
+        src: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275f0442508aceaec02_accel.svg",
+      },
+    ],
+  ];
   return (
     <>
       <section className="h-fit w-[95%] mx-auto pt-20 text-white">
@@ -37,6 +120,17 @@ const Home = () => {
             websites that help brands position themselves as market leaders.
           </p>
         </span>
+      </section>
+      <section id="BrandMarquees" className="mt-[20vh] flex flex-col gap-10">
+        <Marquees
+          marqueeDataWithoutNumber={marqueeDataWithoutNumber[0]}
+          marqueeWithNumber={false}
+        />
+        <Marquees
+          marqueeDataWithoutNumber={marqueeDataWithoutNumber[1]}
+          marqueeWithNumber={false}
+          reverse={true}
+        />
       </section>
     </>
   );
