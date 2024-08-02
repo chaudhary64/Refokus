d3d13ec89d147b9768962061036123ece6a66609
chaudhary64 <chiragchoudhary64@gmail.com>
2024-08-02 15:46:12 +0530

    refactor: Update Positions component layout and styles
    Optimize the layout and styles of the Positions component in the Careers section. This includes adjusting the grid layout to be responsive, updating the font sizes and tracking, and adding a gap between elements. These changes enhance the visual appeal and user experience of the component.

--- src/Components/Carrers/Positions.jsx
+++ src/Components/Carrers/Positions.jsx
@@ -3,16 +3,18 @@ import Button from "../Buttons/Button";
 
 const Positions = ({ title, role }) => {
   return (
-    <div className="h-fit w-[95%] mx-auto py-14 grid grid-cols-3 justify-center items-center border-t-2 border-[#7a7a76] text-white">
-      <span className="text-7xl tracking-wide">{title}</span>
+    <div className="h-fit w-[95%] mx-auto py-8 sm:py-10 md:py-14 grid grid-cols-2 grid-rows-2 md:grid-rows-1 md:grid-cols-3 justify-center items-center gap-[12px_0px] md-gap-[0_0] border-t-2 border-[#7a7a76] text-white">
+      <span className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl tracking-wide">
+        {title}
+      </span>
       <div className="flex flex-col justify-center items-center leading-none">
-        <p className="text-3xl tracking-wider">{role}</p>
+        <p className="text-lg sm:text-2xl lg:text-3xl tracking-wider">{role}</p>
         <div>
           <span>Remote</span>
           <span>Full-time</span>
         </div>
       </div>
-      <span className="justify-self-end">
+      <span className="my-2 md:my-0 col-span-2 md:col-span-1 justify-self-center md:justify-self-end">
         <Button bgColor="transparent" text="Start a project" />
       </span>
     </div>
