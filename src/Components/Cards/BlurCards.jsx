6ab1a9faa1c703230fefabc1c0764b5151ff0bfe
chaudhary64 <chiragchoudhary64@gmail.com>
2024-08-15 16:38:18 +0530

    refactor: Optimize Cards component styles and layout
    Improve the styles and layout of the Cards component by adjusting font sizes, line heights, and text tracking. Make the heading and number responsive, adjusting the font sizes for different screen sizes. Additionally, optimize the text size and spacing in the component to enhance readability and visual appeal. These changes enhance the overall appearance and responsiveness of the Cards component.

--- src/Components/Cards/BlurCards.jsx
+++ src/Components/Cards/BlurCards.jsx
@@ -10,11 +10,13 @@ const BlurCards = ({ heading, number, text, top, left }) => {
         backgroundColor: "rgba(255, 255, 255, .04)",
         boxShadow: "0 25px 500px -12px rgba(255, 255, 255, .08)",
       }}
-      className="min-h-[400px] w-[440px] p-6 grid auto-rows-min item content-evenly border border-[#1C191A] text-white leading-none rounded-3xl absolute z-[2]"
+      className="h-fit sm:h-[250px] md:h-[300px] lg:h-[350px] xl:h-[400px] w-[200px] sm:w-[250px] md:w-[300px] lg:w-[375px] xl:w-[440px] p-6 grid auto-rows-min item content-evenly border border-[#1C191A] text-white leading-none rounded-3xl absolute z-[2]"
     >
-      <p className="text-3xl">{heading}</p>
-      <p className="text-[150px]">{number}</p>
-      <p className="text-xl">{text}</p>
+      <p className="text-base sm:text-xl md:text-2xl lg:text-3xl">{heading}</p>
+      <p className="text-3xl sm:text-5xl md:text-7xl lg:text-[150px]">
+        {number}
+      </p>
+      <p className="text-xs sm:text-base md:text-lg lg:text-xl">{text}</p>
     </div>
   );
 };
