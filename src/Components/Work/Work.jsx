d36bf21400a919a94be2f82229657cd581931729
chaudhary64 <chiragchoudhary64@gmail.com>
2024-09-07 14:00:00 +0530

    Re-added SliderSection component that was mistakenly removed

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -224,6 +224,7 @@ const Work = () => {
         marqueeWithNumber={true}
       />
       {/* <SliderSection /> */}
+      <SliderSection />
       <section
         id="BrandMarquees"
         className="mt-10 sm:mt-12 md:mt-14 lg:mt-16 xl:mt-20 flex flex-col gap-1.5 sm:gap-2.5 md:gap-3 lg:gap-4 xl:gap-4.5"
