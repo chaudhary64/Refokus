109011acf5a97c33fdfbee1fd5a5c549187bbea7
chaudhary64 <chiragchoudhary64@gmail.com>
2024-09-12 12:24:19 +0530

    Enhanced responsiveness of Loader component for various device resolutions

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -39,7 +39,8 @@ const Loader = ({ children }) => {
       </motion.div>
 
       {/* Loader Component */}
-      <motion.span
+      <motion.div
+        id="Loader"
         // Directly using location.pathname will not work as the component is not wrapped in a <Routes> component so we need to pass the location prop from the parent component to this component and then use it to get the pathname of the current route and set the text accordingly in the loader component using the setText function from useState hook. i.e why we are using window.location.pathname in the getRouteName function.
 
         onAnimationStart={() => {
@@ -64,23 +65,25 @@ const Loader = ({ children }) => {
           y: ["-100%", "0%", "0%", "-100%"],
           transition: { duration: 5, times: [0, 0.35, 0.65, 1] },
         }}
-        className="fixed inset-0  p-16 z-[99999999999] bg-black font-[Satoshi-Regular] text-[140px] text-white flex justify-center items-center pointer-events-none"
+        className="fixed inset-0 z-[99999999999] bg-black font-[Satoshi-Regular] text-6xl sm-custom:text-7xl sm:text-[90px] md:text-[105px] lg:text-9xl xl:text-[140px] text-white pointer-events-none flex justify-center items-center"
       >
-        <span className="h-full w-full flex justify-center items-center border-t-[1px] border-b-[1px] border-neutral-600">
-          <p className="leading-none relative pt-5 pr-8 tracking-tight">
-            {text}
-            <motion.span
-              initial={{ backgroundColor: "#a3a08d" }}
-              exit={{
-                backgroundColor: "#Ffd700",
-                boxShadow: "0px 0px 15px 3px #Ffd700",
-                transition: { delay: 2, duration: 0.6 },
-              }}
-              className="inline-block h-[14px] w-[14px] rounded-full absolute right-0 top-0 "
-            ></motion.span>
-          </p>
-        </span>
-      </motion.span>
+        <div className="h-[90%] w-[95%] flex justify-center items-center">
+          <span className="h-full w-full flex justify-center items-center border-t-[1px] border-b-[1px] border-neutral-600">
+            <p className="leading-none relative pt-3 sm-custom:pt-3.5 sm:pt-4 lg:pt-[18px] xl:pt-5 pr-5 sm-custom:pr-4 sm:pr-5 lg:pr-6 xl:pr-8 tracking-wide xl:tracking-tight">
+              {text}
+              <motion.span
+                initial={{ backgroundColor: "#a3a08d" }}
+                exit={{
+                  backgroundColor: "#Ffd700",
+                  boxShadow: "0px 0px 15px 3px #Ffd700",
+                  transition: { delay: 2, duration: 0.6 },
+                }}
+                className="inline-block h-2 sm-custom:h-2.5 sm:h-3 lg:h-[13px] xl:h-[14px] w-2 sm-custom:w-2.5 sm:w-3 lg:w-[13px] xl:w-[14px] rounded-full absolute right-0 top-0 "
+              ></motion.span>
+            </p>
+          </span>
+        </div>
+      </motion.div>
     </>
   );
 };
